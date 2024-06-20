606fba5000c6c35b7c8642a0eb04afbca2c7d8f2
amineohn <amineprojet7@gmail.com>
2024-06-20 21:33:19 +0200

    fix(apps/launchpad): use button element

--- app/components/ui/dock/apps/launchpad/button.tsx
+++ app/components/ui/dock/apps/launchpad/button.tsx
@@ -8,9 +8,9 @@ export function Button({
   label: string;
 }) {
   return (
-    <div key={index} className="flex flex-col items-center">
+    <button key={index} className="flex flex-col items-center">
       <img src={`/static/images/${image}`} className="w-16 h-16" />
       <span className="text-white text-xs font-medium mt-2">{label}</span>
-    </div>
+    </button>
   );
 }
