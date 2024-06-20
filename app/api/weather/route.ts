3521705954e02bd0bcd141c6dafb80b8aa0167f6
amineohn <amineprojet7@gmail.com>
2024-06-20 21:56:38 +0200

    chore(api/weather): add weather api to env and changed it lol

--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
+  const weather_key = process.env.WEATHER_KEY!;
   const weather = axios.get(
-    "https://api.weatherapi.com/v1/current.json?key=f8b5e7f5cd004bcdad4211327240806&q=Bordeaux&aqi=no"
+    `https://api.weatherapi.com/v1/current.json?key=${weather_key}&q=Bordeaux&aqi=no`
   );
   const response = await weather;
   return NextResponse.json(response.data);
