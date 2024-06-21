58b916ea45ff27787bd9f77052834e19cfbab264
amineohn <amineprojet7@gmail.com>
2024-06-21 23:58:27 +0200

    refactor(screens/loading): better animation while loading leave

--- app/components/screens/loading.tsx
+++ app/components/screens/loading.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import classNames from "classnames";
 import { useEffect, useState } from "react";
 import { Icons } from "~/app/components/ui/icons";
 import { motion } from "framer-motion";
@@ -32,36 +33,38 @@ export function LoadingScreen() {
     if (showLoading) {
       const interval = setInterval(() => {
         setProgress((prevProgress) => Math.min(prevProgress + 10, 100));
-      }, 700);
+      }, 500);
 
       return () => clearInterval(interval);
     }
   }, [showLoading]);
 
-  const variants = {
-    initial: { opacity: 0 },
-    animate: { opacity: 1 },
-    exit: { opacity: 0 },
-  };
+  const loadingClasses = classNames("", {
+    "flex flex-col space-y-10 justify-center items-center h-screen bg-black transition-all ease-in-out overflow-hidden z-50 removable":
+      showLoading,
+    hidden: !showLoading,
+  });
 
-  return showLoading ? (
-    <motion.div
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      variants={variants}
-      className="flex flex-col space-y-10 justify-center items-center h-screen bg-black transition-all ease-in-out"
-      transition={{ duration: 0.5, ease: "easeInOut" }}
-    >
-      <Icons name="apple" className="w-36 h-36 text-white opacity-100" />
-      <div className="w-64 h-1.5 bg-white/50 rounded-full overflow-hidden">
-        <div
-          className="h-full bg-white rounded-full"
-          style={{ width: `${progress}%`, transition: "width 1s ease-in-out" }}
-        ></div>
-      </div>
-    </motion.div>
-  ) : (
-    <MainContent />
+  return (
+    <>
+      <motion.div
+        className={loadingClasses}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: showLoading ? 1 : 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Icons name="apple" className="w-36 h-36 text-white opacity-100" />
+        <div className="w-64 h-1.5 bg-white/50 rounded-full overflow-hidden">
+          <div
+            className="h-full bg-white rounded-full"
+            style={{
+              width: `${progress}%`,
+              transition: "width 1s ease-in-out",
+            }}
+          ></div>
+        </div>
+      </motion.div>
+      {!showLoading && <MainContent />}
+    </>
   );
 }
