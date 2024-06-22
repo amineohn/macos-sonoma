a6c5e4fb2d80aaeb5eaee6e4a03c13094db8f66d
amineohn <amineprojet7@gmail.com>
2024-06-22 12:02:34 +0200

    refactor(ui/icons): added svg into component

--- app/components/ui/control-center/index.tsx
+++ app/components/ui/control-center/index.tsx
@@ -2,6 +2,7 @@
 import { IoIosMoon } from "react-icons/io";
 import { LuAirplay } from "react-icons/lu";
 import { items } from "~/app/components/ui/control-center/control-center.data";
+import { Icons } from "../icons";
 
 export function ControlCenter({ open }: { open: boolean }) {
   return (
@@ -36,27 +37,8 @@ export function ControlCenter({ open }: { open: boolean }) {
             </div>
             <div className="flex space-x-2">
               <div className="w-20 flex flex-col h-20 justify-center bg-white/20 items-center rounded-xl">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <rect x="3" y="10" width="18" height="4" rx="1" ry="1"></rect>
-                  <line x1="12" y1="2" x2="12" y2="4"></line>
-                  <line x1="12" y1="20" x2="12" y2="22"></line>
-                  <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-                  <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-                  <line x1="1" y1="12" x2="3" y2="12"></line>
-                  <line x1="21" y1="12" x2="23" y2="12"></line>
-                  <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-                  <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-                </svg>
+                <Icons name="keyboard-bright" className="w-6 h-6" />
+
                 <p className="text-xs text-center font-medium">
                   Keyboard Brightness
                 </p>
