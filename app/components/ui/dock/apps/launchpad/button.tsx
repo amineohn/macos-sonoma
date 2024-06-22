335083d4877503ccf3225fed2c7c339ee7bed52d
amineohn <amineprojet7@gmail.com>
2024-06-22 14:59:48 +0200

    fix(apps/launchpad): issue with launchpad when exit, feat(apps/launchpad): search apps in launchpad, refactor(launchpad/buttons): add button attr to get other properties

--- app/components/ui/dock/apps/launchpad/button.tsx
+++ app/components/ui/dock/apps/launchpad/button.tsx
@@ -1,11 +1,18 @@
-interface LaunchPadButtonProps {
+import { ButtonHTMLAttributes } from "react";
+
+interface LaunchPadButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   index: number;
   image: string;
   label: string;
 }
-export function Button({ index, image, label }: LaunchPadButtonProps) {
+export function Button({
+  index,
+  image,
+  label,
+  ...props
+}: LaunchPadButtonProps) {
   return (
-    <button key={index} className="flex flex-col items-center">
+    <button {...props} key={index} className="flex flex-col items-center">
       <img src={`/static/images/${image}`} className="w-16 h-16" />
       <span className="text-white text-xs font-medium mt-2">{label}</span>
     </button>
--- app/components/ui/dock/apps/launchpad/index.tsx
+++ app/components/ui/dock/apps/launchpad/index.tsx
@@ -10,6 +10,7 @@ interface LaunchPadProps {
 
 export function LaunchPad({ openLaunchPad }: LaunchPadProps) {
   const [isOpen, setIsOpen] = useState(openLaunchPad);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     setIsOpen(openLaunchPad);
@@ -29,11 +30,15 @@ export function LaunchPad({ openLaunchPad }: LaunchPadProps) {
     };
   }, []);
 
+  const filteredMenuItems = menuItems.filter((item) =>
+    item.label.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   const launchpadClasses = classNames(
     "bg-black/50 backdrop-filter backdrop-blur-xl absolute inset-0 launchpad",
     {
       "overflow-hidden z-50": isOpen,
-      "z-0 hidden": !isOpen,
+      "z-0": !isOpen,
     }
   );
 
@@ -45,36 +50,51 @@ export function LaunchPad({ openLaunchPad }: LaunchPadProps) {
         animate={{ opacity: isOpen ? 1 : 0 }}
         transition={{ duration: 0.5 }}
       >
-        <div className="flex justify-center flex-col items-center py-10">
-          <div className="flex items-center">
-            <div className="relative hidden">
-              <input
-                type="text"
-                className="outline-none bg-white/10 rounded-md py-[1px] w-80 text-center font-normal text-white placeholder-white/60 border border-white/30 pl-8"
-                placeholder="Search"
+        <div
+          className={classNames(
+            "flex justify-center flex-col items-center py-10 select-none",
+            {
+              hidden: !isOpen,
+            }
+          )}
+        >
+          <div className="flex items-center bg-transparent border border-white/25 rounded-md px-1 py-0.5 w-60 max-w-xs select-none">
+            <svg
+              className="h-5 w-5 text-white/75 ml-0.5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1111.24 3.37l4.16 4.16a1 1 0 01-1.41 1.41l-4.16-4.16A6 6 0 012 8z"
+                clipRule="evenodd"
               />
-              <svg
-                className="w-4 h-4 absolute top-1/2 left-[8rem] transform -translate-y-1/2 text-white/60 stroke-white/60"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
-                  stroke="currentStroke"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </div>
+            </svg>
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="bg-transparent text-xs text-white/75 placeholder:text-white/75 focus:outline-none ml-2 w-full"
+            />
           </div>
         </div>
 
-        <div className="container mx-auto max-w-9xl mt-20">
+        <div
+          className={classNames("container mx-auto max-w-9xl mt-20 ", {
+            hidden: !isOpen,
+          })}
+        >
           <div className="grid grid-cols-7 gap-3">
-            {menuItems.map((item, index) => (
-              <Button index={index} image={item.image} label={item.label} />
+            {filteredMenuItems.map((item, index) => (
+              <Button
+                key={index}
+                className="select-none"
+                index={index}
+                image={item.image}
+                label={item.label}
+              />
             ))}
           </div>
         </div>
